9727e5222e5cc6cddc5458f0bee8a627e48873cd
Aleksis Rautiainen <aleraut9@gmail.com>
2023-01-31 10:45:43 +0200

    Tehtävä4

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo('');
+  };
+
+  const listSeparator = () => {
+    return(
+      <View style={{ height: 1, backgroundColor: 'blue' }} />
+    );
+  };
+
+  const clearList = () => {
+    setTodos([]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput 
+        value={todo}
+        onChangeText={text => setTodo(text)}
+        style={{ width: 200, borderColor: 'gray', borderWidth: 1,}}
+      />
+      <View style={styles.buttons}>
+      <Button title='Add' onPress={addTodo} />
+      <Button title='Clear' onPress={clearList} />
+      </View>
+      <Text style={{ fontSize: 20, color: 'blue', fontWeight: 'bold' }}>Shopping List</Text>
+      <FlatList 
+        data={todos}
+        renderItem={({item}) => <Text style={{ fontSize: 20 }}>{item}</Text>}
+        ItemSeparatorComponent={listSeparator}
+      />
     </View>
   );
 }
@@ -16,5 +47,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 80,
+  },
+  buttons: {
+    flexDirection: "row",
+    margin: 20,
   },
 });
